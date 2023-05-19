48d97230062fe6ad0fea720ec1912b517f3ac3d6
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-05-19 22:13:19 +0300

    task-1 ready

---
+++ src/Clock.jsx
@@ -0,0 +1,11 @@
+export default function Clock({time}) {
+    const hours = time.getHours();
+
+    const className = hours >= 0 && hours <= 6 ? 'night' : 'day'
+
+    return (
+        <h1 className={className}>
+            {time.toLocaleTimeString()}
+        </h1>
+    )
+}
